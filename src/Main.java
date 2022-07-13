4d945de3bd41c94d3063e1ead0662f0224d3ef8b
Alex080190 <Alex080190@mail.ru>
2022-07-13 15:46:32 +0300

    Add src

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Troll.digital_root(458));
+    }
+}
+
+class Troll {
+
+    public static int digital_root(int n) {
+
+        if (n < 10)
+            return n;
+        else {
+            return digital_root(Integer.sum(n % 10, n / 10));
+        }
+    }
+}
